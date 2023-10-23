9790601b13e979cd82dde65279fe05787ce666af
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-10-23 21:00:15 +0300

    `feat:` add `alpha` and `beta`

--- network.py
+++ network.py
@@ -7,9 +7,12 @@ def sigmoid(x, deriv=False):
     return 1 / (1+np.exp(-x))
 
 class Perceptron:
-    def __init__(self, x, g, y):
+    def __init__(self, x, g, y, alpha, beta):
         self.v = 2*np.random.random((g, x)) - 1
         self.w = 2*np.random.random((y, g)) - 1
 
         self.q = 2*np.random.random((1, g)) - 1
         self.t = 2*np.random.random((1, y)) - 1
+
+        self.alpha = alpha
+        self.beta = beta
