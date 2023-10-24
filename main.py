6e134329d8ae8b629e36473cc6b4350f098e8e89
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-10-24 07:52:29 +0300

    `feat:` implement `binarize` function

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import numpy as np
+
 from ideal import TWO, THREE, FOUR, FIVE, SEVEN
 from network import Perceptron
+
+def binarize(image):
+    result = image.copy()
+
+    for i, x in np.ndenumerate(result):
+        result[i] = (255-x) / 255
+
+    return result.reshape((result.size, 1))
 
 def main():
     percept = Perceptron(36, 20, 5)
