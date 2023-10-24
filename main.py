a855729707d4a86f89e10837c047ba069ed49fb8
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-10-24 16:32:51 +0300

    `feat!:` implement perceptron learning

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 from ideal import TWO, THREE, FOUR, FIVE, SEVEN
 from network import Perceptron
 
+EPSILON = 0.001
+
 def noise(image, chance):
     """
     Returns a noisy `image` determined by `chance`.
@@ -39,6 +41,29 @@ def main():
         (binarize(FIVE).T,  np.array([[0, 0, 0, 1, 0]]).T),
         (binarize(SEVEN).T, np.array([[0, 0, 0, 0, 1]]).T),
     ]
+
+    n = len(training_data)
+
+    # Total number of iterations.
+    epochs = 0
+
+    while True:
+        np.random.shuffle(training_data)
+
+        percept.gradient_descent(training_data)
+
+        # Error function of the `percept`.
+        delta = np.zeros((n, 1))
+
+        for data in training_data:
+            delta += (data[1]-percept.feed_forward(data[0])) ** 2
+
+        if np.all(delta/n < EPSILON):
+            break
+
+        epochs += 1
+
+    print(f'epochs = {epochs}')
 
 if __name__ == '__main__':
     main()
