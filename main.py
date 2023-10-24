ba558d8ad9dddeb03154d441882aa83c34c584fb
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-10-24 09:21:16 +0300

    `feat:` implement `noize` function

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ import numpy as np
 from ideal import TWO, THREE, FOUR, FIVE, SEVEN
 from network import Perceptron
 
+def noize(image, delta):
+    result = image.copy()
+
+    for i, x in np.ndenumerate(result):
+        if x == 255:
+            x -= np.random.randint(0, delta)
+        else:
+            x += np.random.randint(0, delta)
+
+        result[i] = x
+
+    return result
+
 def binarize(image):
     result = image.copy()
 
